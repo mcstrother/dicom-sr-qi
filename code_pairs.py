cd595f11f7304a40cbb9f8a36a03daf4f4e7d9ec
mstrot01 <mstrot01@MIR1MIR05IVR045.mir.wucon.wustl.edu>
2011-06-17 10:00:58 -0500

    code_pairs works with string cpts. added prints

--- code_pairs.py
+++ code_pairs.py
@@ -12,8 +12,8 @@ def get_code_pairs(in_file_name):
     for sheet in code_pairs_wb.sheets():
         code_pairs[sheet.name] = {}
         code_pairs[sheet.name]['separation'] = datetime.timedelta(days = float(sheet.cell(0,1).value))
-        code_pairs[sheet.name]['combo1'] = [int(x.value) for x in sheet.row(1)[1:]]
-        code_pairs[sheet.name]['combo2'] = [int(x.value) for x in sheet.row(2)[1:]]
+        code_pairs[sheet.name]['combo1'] = [my_utils.standard_cpt(x.value) for x in sheet.row(1)[1:]]
+        code_pairs[sheet.name]['combo2'] = [my_utils.standard_cpt(x.value) for x in sheet.row(2)[1:]]
     return code_pairs
 
 import os
@@ -26,27 +26,36 @@ def get_syngo_file_names():
             out.append(f)
     return out
 
+print "Parsing files..."
 procs = Parse_Syngo.parse_syngo_files(get_syngo_file_names())
-procs.sort(key= lambda x:x.dos)#sort procs by start time
+print "Done"
+procs.sort(key= lambda x:x.dos_start)#sort procs by start time
 cps = get_code_pairs(PAIR_FILE_NAME)
 
 out = {}
 for sheet_name, spec in cps.iteritems():
+    print "Working on code pair " + sheet_name
+    print "Finding matches for code combinations..."
     combo1_matches = [x for x in procs if my_utils.matches(spec['combo1'],x.cpts)]
     combo2_matches = [x for x in procs if my_utils.matches(spec['combo2'],x.cpts)]
+    print "Done."
+    print "Sorting by patient identifier..."
     lookup = {}#comob2_matches sorted by mpi
     for proc in combo2_matches:
         if not proc.mpi in lookup:
             lookup[proc.mpi] =[]
         lookup[proc.mpi].append(proc)
+    print "Done."
+    print "Finding pairs within time span..."
     out[sheet_name] = []
     for proc in combo1_matches:
         if not proc.mpi in lookup:
             continue
         for proc2 in lookup[proc.mpi]:
-            if proc2.dos > proc.dos and (proc2.dos -proc.dos) < spec['separation']:
+            if proc2.dos_start > proc.dos_start and (proc2.dos_start -proc.dos_start) < spec['separation']:
                 out[sheet_name].append(proc)
                 out[sheet_name].append(proc2)
+    print "Done."
 
 
 Parse_Syngo.write_syngo_file('output.xls',out )                    
