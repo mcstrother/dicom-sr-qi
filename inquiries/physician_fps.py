5b195278bf123c18870930aa3edf7b733fdc03a8
mcstrother <mcstrother@localhost>
2011-06-27 17:52:10 -0500

    added methods for getting combined data in physician_fps.py

--- inquiries/physician_fps.py
+++ inquiries/physician_fps.py
@@ -3,21 +3,24 @@ import matplotlib.pyplot as plt
 import os
 
 import mirqi.core.assess_procedure
+from mirqi.core import my_utils
 
 class Physician_FPS(mirqi.core.assess_procedure.Inquiry):
 
-    DAYS_PER_PERIOD = 14
+    DAYS_PER_PERIOD = 7
     #TODO implement parameters for start and end day
     
     def run(self, procs, context):
         DAYS_PER_PERIOD = 14
-        procs = [p for p in procs if p.is_valid() and p.is_real() and p.has_syngo()]
+        procs = [p for p in procs if p.is_pure()]
         procs_by_attending = my_utils.organize(procs, lambda x: x.get_syngo().rad1.replace(',',''))
 
         first_time = min(procs, key = lambda x: x.get_start_time()).get_start_time()
         last_start_time = max(procs, key = lambda x: x.get_start_time()).get_start_time()
         self.num_periods = int((last_start_time - first_time).days/DAYS_PER_PERIOD)
-
+        self.first_time = first_time
+        self.last_start_time =last_start_time
+        
         out = {}
         for attending, procs in procs_by_attending.iteritems():
             events = [p.get_events() for p in procs]
@@ -27,16 +30,30 @@ class Physician_FPS(mirqi.core.assess_procedure.Inquiry):
                                                  lambda e: int((e.get_start_time() - first_time).days/DAYS_PER_PERIOD))
             out[attending] = events_by_period            
 
-        self.lookup = out #lookup[attending][period_number] --> event
+        self.lookup = out #lookup[attending][period_number] --> [events]
 
+    def _get_combined(self):
+        """Get average and counts for each period
+        combined across physicians
+        """
+        events_by_period = []
+        for period in range(self.num_periods):
+            events = []
+            for attending in self.lookup.keys():
+                if period in self.lookup[attending]:
+                    events = events + self.lookup[attending][period]
+            events_by_period.append(events)
+        counts_by_period = [len(events) for events in events_by_period]
+        averages_by_period = [my_utils.average_fps(events) for events in events_by_period]
+        return counts_by_period, averages_by_period
 
-    def get_table(self):
+    def _get_individual_table(self):
         attending_list = sorted(self.lookup.keys())
         dimension = (self.num_periods ,len(attending_list))
         average_table =np.zeros(dimension).tolist()
         count_table = np.zeros(dimension).tolist()
         for a, attending in enumerate(attending_list):
-            for period in range(self.num_periods):
+            for period in range(self.num_periods-1):
                 if period in self.lookup[attending]:
                     events = self.lookup[attending][period]
                     count = len(events)
@@ -53,11 +70,33 @@ class Physician_FPS(mirqi.core.assess_procedure.Inquiry):
         out = my_utils.transposed(out)
         return out
 
-    def get_figure(self):
+    def get_table(self):
+        #return self._get_individual_table()
+        return self._get_combined_table()
+
+    def _get_combined_table(self):
+        counts, averages = self._get_combined()
+        heading = [[''] + range(len(counts))]
+        row1 = [['Averages'] + averages]
+        row2 = [['Counts'] + counts]
+        return heading+row1+row2
+
+    def _get_combined_figure(self):
+        counts, averages = self._get_combined()
+        fig = plt.figure(1)
+        plt.scatter(range(len(averages)), averages, s= counts)
+        plt.plot(range(len(averages)), averages, color='red')
+        plt.xlabel('Period Number')
+        plt.ylabel('Average FPS')
+        plt.title("Average FPS Across All Events")
+        plt.axis([0,self.num_periods-1,5,15])
+        return fig
+
+    def _get_individual_figure(self):
         num_attendings = len(self.lookup.keys())
         fig = plt.figure(1)
         for a, attending in enumerate(sorted(self.lookup.keys())):
-            plt.subplot(num_attendings, 1,a)
+            plt.subplot(num_attendings+1, 1,a)
             plt.axis([0,self.num_periods,5,15])
             plt.xlabel('Period Number')
             plt.ylabel('Average FPS')
@@ -71,18 +110,26 @@ class Physician_FPS(mirqi.core.assess_procedure.Inquiry):
                 s.append(len(events))
             plt.scatter(x,y,s=s,label=attending)
             plt.plot(x,y,color='red')
+        plt.plot(num_attendings+1, 1, num_attendings+1)
         return fig
+
+    def get_figure(self):
+        #return self._get_individual_figure()
+        return self._get_combined_figure()
 
     def get_name(self):
         return u'Physician FPS'
 
-
+    def get_text(self):
+        out = "First event at " + str(self.first_time) + "\n"
+        out += "Last event at " + str(self.last_start_time) + "\n"
+        return out 
 
 from mirqi.gui import report_writer
 from mirqi.core import my_utils
 
 if __name__ == '__main__':
-    procs = [p for p in my_utils.get_procs('test') if p.is_pure()]
+    procs = [p for p in my_utils.get_procs('bjh') if p.is_pure()]
     inq = Physician_FPS(procs)
     report_writer.write_report([inq])
 
