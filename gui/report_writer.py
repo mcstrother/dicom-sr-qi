729b2d16f68b088375d5e1c15ebee7a142244cfc
mcstrother <mcstrother@localhost>
2011-06-27 13:56:50 -0500

    first commit of functional report_writer working with physician_fps

--- gui/report_writer.py
+++ gui/report_writer.py
@@ -1,8 +1,9 @@
 import os.path as path
 import jinja2
+import mirqi
 
 def _get_report_template():
-    TEMPLATE_FOLDER = "C:\Users\mcstrother\Documents\Duncan Research\mirqi\gui\templates"
+    TEMPLATE_FOLDER = path.join(mirqi.gui.__path__[0], 'templates')
     TEMPLATE_NAME = 'report.html'
     template_path = path.join(TEMPLATE_FOLDER,TEMPLATE_NAME)
     with open(template_path, 'r') as f:
@@ -11,9 +12,10 @@ def _get_report_template():
 
 
 def write_report(inqs):
-    OUTPUT_FOLDER = "C:\Users\mcstrother\Documents\Duncan Research\mirqi"
+    OUTPUT_FOLDER = mirqi.__path__[0]
     OUTPUT_NAME = 'output.html'
     output_path = path.join(OUTPUT_FOLDER, OUTPUT_NAME)
+    template = _get_report_template()
     with open(output_path, 'w') as f:
         f.write(template.render(inquiries= inqs))
 
