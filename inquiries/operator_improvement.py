ee57486d6c44e41bfeed50125a36a86ab5a367b3
mcstrother <mcstrother@localhost>
2011-07-07 14:14:56 -0500

    full functional operator_improvement.py with test case and test data

--- inquiries/operator_improvement.py
+++ inquiries/operator_improvement.py
@@ -8,31 +8,14 @@ class Operator_Improvement(inquiry.Inquiry):
                                          "The minimum number of times a procedure with the same CPT codes must occur to be considered to have a reasonable distribution")
     PROCS_PER_WINDOW = inquiry.Inquiry_Parameter(50, "Procedures Per Window",
                                                  "Number of procedures that should be considered in the sliding window calculation of the operators performance metric.")
-    MIN_PROCS_PER_ATTENDING = inquiry.Inquiry_Parameter(52, "Minimum procedures per attending",
-                                                        "The minimum number of useable procedures (data points on the final graph) that an attending has to do to appear. Must be >= Procedures per window +1")
-
     def run(self, procs, context, extra_procs):
-        # extract all the syngo procedures from procs and extra_procs
-        #
-        syngo_procs = [p for p in extra_procs if type(p) == Parse_Syngo.Syngo]
-        for proc in procs:
-            if proc.has_syngo():
-                syngo_procs.append(proc.get_syngo())
-        syngo_procs = [p for p in syngo_procs if not p.fluoro is None]
-        cpt_to_procs = my_utils.organize(syngo_procs, lambda p: p.get_cpts_as_string())
-        #remove procedures with less than MIN_REPS.value instances of same cpt code
-        to_remove = []
-        for k, v in cpt_to_procs.iteritems():
-            if len(v) < self.MIN_REPS.value:
-                to_remove.append(k)
-        for cpt in to_remove:
-            del cpt_to_procs[cpt]
+        cpt_to_procs = self._get_procedures_helper(procs, extra_procs)
         # calculate statistics for each procedure type
         medians = {}
         std_devs = {}
         for cpt, p_list in cpt_to_procs.iteritems():
             fluoro_list = [p.fluoro for p in p_list]
-            medians[cpt] = np.median(fluoro_list)
+            medians[cpt] = float(np.median(fluoro_list))
             std_devs[cpt] = np.std(fluoro_list)
         # organize by rad1 and sort by date
         rad1_to_procs = my_utils.organize(sum(cpt_to_procs.values(),[]), lambda p:p.rad1)
@@ -40,28 +23,64 @@ class Operator_Improvement(inquiry.Inquiry):
             p_list.sort(key = lambda p:p.dos_start)
         # remove rads with too few procedures
         for rad1 in rad1_to_procs.keys():
-            if len(rad1_to_procs[rad1]) < self.MIN_PROCS_PER_ATTENDING.value:
+            if len(rad1_to_procs[rad1]) < (self.PROCS_PER_WINDOW.value +2):#have to be able to plot at least 3 points
                 del rad1_to_procs[rad1]
+        self._the_meat(rad1_to_procs, medians)
+
+
+    def _get_procedures_helper(self, procs, extra_procs):
+        """Extract all the Syngo procedures that we're interested in
+        (i.e. all the ones that have enough repetitions of the same procedure)
+        and return them as a dictionary mapping cpt code combinations to
+        lists of procedures
+
+        Arguments:
+            procs : a list of (SR) Procedure objects
+            extra_procs : a list of non-SR procedure objects that could not
+                be assigned to SR procedures
+        """
+        # extract all the syngo procedures from procs and extra_procs
+        syngo_procs = [p for p in extra_procs if type(p) == Parse_Syngo.Syngo]
+        for proc in procs:
+            if proc.has_syngo():
+                syngo_procs.append(proc.get_syngo())
+        #remove procs without a fluoro number entered
+        syngo_procs = [p for p in syngo_procs if not p.fluoro is None]
+        #remove procedures with less than MIN_REPS.value instances of same cpt code
+        cpt_to_procs = my_utils.organize(syngo_procs, lambda p: p.get_cpts_as_string())
+        for k in cpt_to_procs.keys():
+            if len(cpt_to_procs[k]) < self.MIN_REPS.value:
+                del cpt_to_procs[k]
+        return cpt_to_procs
+        
+        
+    
+    def _the_meat(self, rad1_to_procs, medians):
+        """Set self.lookup, which is the meat of self.run
+        Everything else in self.run is basically preprocessing
+
+        Arguments:
+            rad1_to_procs : dictionary mapping rad1 (a string) to a list of
+                Syngo procedures
+            medians : dictionary mapping a cpt code set (a string) to a float
+        """
         # calculate metrics
         rad1_to_out = {}
         for rad1, p_list in rad1_to_procs.iteritems():
             out = []
             metric_queue = collections.deque()
             cum_metric = 0
-            for i in range(len(p_list)):
-                fluoro = float(p_list[i].fluoro)
-                med = medians[p_list[i].get_cpts_as_string()]
-                std_dev = std_devs[p_list[i].get_cpts_as_string()]
-                if std_dev == 0:
-                    metric =0
-                else:
-                    metric =(fluoro-med)#/std_dev
+            for i, proc in enumerate(p_list):
+                fluoro = float(proc.fluoro)
+                med = medians[proc.get_cpts_as_string()]
+                metric =(fluoro-med)#/std_dev
                 cum_metric += metric
                 metric_queue.append(metric)
-                if i >= self.PROCS_PER_WINDOW.value-1:
-                    cum_metric -= metric_queue.popleft()
-                    if i == len(p_list)-1 or not p_list[i].dos_start == p_list[i+1].dos_start:
-                        out.append((p_list[i].dos_start,cum_metric))
+                if len(metric_queue)>=self.PROCS_PER_WINDOW.value:
+                    if len(metric_queue) > self.PROCS_PER_WINDOW.value:
+                        cum_metric -= metric_queue.popleft()
+                    if i == len(p_list)-1 or not (proc.dos_start == p_list[i+1].dos_start):
+                        out.append((proc.dos_start, cum_metric))
             rad1_to_out[rad1] = out
         self.lookup = rad1_to_out #rad1_to_out[rad1][window_number] = (date, metric value)
         
