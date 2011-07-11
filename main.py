8d5df0fb6038eca6a94eb2dbd9adf04f2334776e
mcstrother <mcstrother@localhost>
2011-07-11 14:49:14 -0500

    adding new, overall main. also adding test/__init__.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+def main():
+    folder_name = os.path.dirname(os.getcwd())
+    if not folder_name in sys.path:
+        sys.path.append(folder_name)
+    from mirqi.gui import main as gui_main
+    gui_main.main()
+
+if __name__ == "__main__":
+    main()
+
+
