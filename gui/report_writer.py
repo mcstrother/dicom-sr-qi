b7ad6cd48e84ab49afaa9c4caeef8c86cc2a5a11
mcstrother <mcstrother@localhost>
2011-07-19 16:30:07 -0500

    fixed bug in report_writer.py that caused it to fail if you delete output.html (but not if you delete both output.html and its containing folder)

--- gui/report_writer.py
+++ gui/report_writer.py
@@ -74,8 +74,8 @@ class Report_Writer(object):
     def write(self, template_path = _default_template_path,
               output_path = _default_out_path):
         template = self._get_template(template_path)
-        if not os.path.exists(output_path):
-            os.mkdir(os.path.dirname(output_path))
+        if not os.path.exists(os.path.dirname(output_path)):
+            os.makedirs(os.path.dirname(output_path))
         with open(output_path, 'w') as f:
             f.write(template.render(inquiries= self.inqs))
         
