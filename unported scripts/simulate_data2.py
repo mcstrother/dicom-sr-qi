f2db0beea40ed1b659bcbacfa1df13e530d197ad
mcstrother <mcstrother@localhost>
2011-07-26 14:13:18 -0500

    added more options to simulate_data2.py

--- unported scripts/simulate_data2.py
+++ unported scripts/simulate_data2.py
@@ -5,6 +5,12 @@ sys.path.append(srqi_containing_dir)
 from srqi.core import my_utils
 import numpy as np
 import math
+from datetime import date
+
+SIMULATION = 'july'
+#SIMULATION = 'improvement'
+JULY_EFFECT = .5
+WINDOW_SIZE = 50
 
 def add_new_fellow_factor(proc):
     start_date = p.get_start_date()
@@ -24,14 +30,19 @@ def main():
         if p.rad1 == 'SAAD, N.':
             p.rad1 = 'Simulated'
             orig_fluoro =p.fluoro
-            p.fluoro = p.fluoro * (1.0 - .2*i/len(syngo_procs))
-            if i%100 == 0:
-                print (orig_fluoro, p.fluoro) 
+            if SIMULATION == 'improvement':
+                #physician improves 20% over the course of the time period
+                p.fluoro = p.fluoro * (1.0 - .2*i/len(syngo_procs))
+            elif SIMULATION == 'july':
+                july = date(p.get_start_date().year, 7,1)
+                days = (p.get_start_date() - july).days #days since july 1
+                if days > 0:
+                    p.fluoro = p.fluoro * (1 + JULY_EFFECT - JULY_EFFECT * days/182)
 
     from srqi.inquiries.operator_improvement import Operator_Improvement
     import matplotlib.pyplot as plt
     oi_cls = Operator_Improvement
-    oi_cls.PROCS_PER_WINDOW.set_value(400)
+    oi_cls.PROCS_PER_WINDOW.set_value(WINDOW_SIZE)
     oi = oi_cls([], [], syngo_procs)
     oi.get_figures()
     plt.show()  
