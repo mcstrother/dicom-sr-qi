8fec114d845f4bce3d498c41fba770f6180ae77d
mcstrother <mcstrother@localhost>
2011-12-29 12:49:37 -0600

    pulled a few more aggregate data points over with combine_sr_syngo.py

--- inquiries/combine_sr_syngo.py
+++ inquiries/combine_sr_syngo.py
@@ -9,7 +9,8 @@ class Combine_Sr_Syngo(inquiry.Inquiry):
     def get_tables(self):
         #get the standard heading list except for the last entry, which is 'CPTs'
         headings = self.sr_procs[0].get_syngo().get_heading_list()[:-1]
-        headings += ["Total Dose (Gy) (SR)", "Total DAP (Gym2) (SR)", "Pedal Time (s) (SR)"]
+        headings += ["Total Dose (Gy)(SR)", "Total DAP (Gym2)(SR)", "Pedal Time (s)(SR)",
+                     "Fluoro Dose (Gy)(SR)", "Fluoro DAP (Gym2)(SR)", "Fluoro Exposure Time (ms)"]
         headings += ["CPTs"]
         table = [headings]
         for sr_proc in self.sr_procs:
@@ -21,8 +22,12 @@ class Combine_Sr_Syngo(inquiry.Inquiry):
             total_Dose = sr_proc.get_total_Dose()
             total_DAP = sr_proc.get_total_DAP()
             pedal_time = sr_proc.get_pedal_time()
+            total_fluoro_DAP = sum([e.Dose_Area_Product for e in sr_proc.get_fluoro_events()])
+            total_fluoro_dose = sum([e.Dose_RP for e in sr_proc.get_fluoro_events()])
+            total_fluoro_time = sum([e.Exposure_Time for e in sr_proc.get_fluoro_events()])
             #put the SR data in the row with the Syngo data
-            row += [total_Dose, total_DAP, pedal_time]
+            row += [total_Dose, total_DAP, pedal_time,
+                    total_fluoro_dose, total_fluoro_DAP, total_fluoro_time]
             #tack the CPTs from the Syngo data on to the end
             row += [sr_proc.get_syngo().get_cpts_as_string()]
             table.append(row)
